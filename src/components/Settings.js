2a840659ff1bdcd6d6002151c46985e4dee2292d
Safal Pandey <safal.pandey.sp@gmail.com>
2021-02-28 22:45:30 +0545

    Create separate component for settings

---
+++ src/components/Settings.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Settings({ settings, setSettings }) {
+  return (
+    <>
+      <h2>Settings</h2>
+      <div id="settings" style={{ width: '80%' }}>
+        <input
+          type="checkbox"
+          id="display-meaning-setting"
+          onChange={(e) => setSettings({ shouldShowMeaning: e.target.checked })}
+          checked={settings.shouldShowMeaning}
+        />
+        <label htmlFor="display-meaning-setting">Display Meaning of current word</label>
+      </div>
+      <br />
+    </>
+  );
+}
+
+export default Settings;
