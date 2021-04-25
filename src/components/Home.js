44acfcb0de2b53012d11b5680c790cd9b23faa89
Safal Pandey <safal.pandey.sp@gmail.com>
2021-04-25 22:56:41 +0545

    Create separate parseCustomWords service with parsing and validation logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -77,4 +77,41 @@ function Home() {
   );
 }
 
+/**
+ * Parses and validates the passed custom words and throws appropriate errors.
+ *
+ * @param {string} customWords
+ * @param {boolean} shouldShowMeaning
+ * @returns {object}
+ */
+function parseCustomWords(customWords, shouldShowMeaning) {
+  let parsedCustomWords;
+
+  try {
+    parsedCustomWords = JSON.parse(customWords);
+  } catch (e) {
+    throw new Error('Passed data must be a valid JSON Array. Could not parse passed list.');
+  }
+
+  if (!Array.isArray(parsedCustomWords)) {
+    throw new Error('Custom words needs to be an array.');
+  }
+
+  for (let { word, meaning } of parsedCustomWords) {
+    if (!word) {
+      throw new Error(
+        `Every element in passed data must have a "word" ${shouldShowMeaning ? 'as well as a "meaning"' : ''} property.`
+      );
+    }
+
+    if (shouldShowMeaning && !meaning) {
+      throw new Error(
+        `Every element in passed data must have a "meaning" property. Turn off "Display meaning of current word" setting to continue without "meaning" property.`
+      );
+    }
+  }
+
+  return parsedCustomWords;
+}
+
 export default Home;
